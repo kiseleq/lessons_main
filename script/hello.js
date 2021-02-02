53ad873e4e93f76a5eab54cd5419a32368a336c2
Kiselev Andrey <kiseleq@gmail.com>
2021-02-02 16:23:20 +0300

    Урок 3 поправил

--- script/hello.js
+++ script/hello.js
@@ -1,5 +1,5 @@
 'use strict';
-const money = prompt('Ваш месячный доход?'),
+const money = +prompt('Ваш месячный доход?'),
     addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
     mission = 6305,
     deposit = confirm('Есть ли у вас дпозит в банке?'),
@@ -7,27 +7,10 @@ const money = prompt('Ваш месячный доход?'),
     amount1 = +prompt ('Во сколько это обойдется?'),
     expenses2 = prompt('Введите обязательную статью расходов'),
     amount2 = +prompt ('Во сколько это обойдется?'),
+    budgetMonth = money - amount1 - amount2,
     income = 'Макдональдс',
     period = 5,
-
-    getExpensesMonth = function() {
-        return amount1 + amount2;
-    },
-    getAccumulatedMonth = function () {
-        return money - getExpensesMonth();
-    },
-    accumulatedMonth = getAccumulatedMonth(),
-    getTargetMonth = function () {
-        return mission / accumulatedMonth;
-    },
-    showTypeOf = function(varible) {
-        return console.log(typeof varible);
-    },
-    getStatusIncome = function () {
-        console.log(income);
-    },
-
-    budgetDay = getAccumulatedMonth() / 30;
+    budgetDay = budgetMonth / 30;
 
 if (budgetDay >= 1200) {
     console.log('У вас высокий уровень дохода');
@@ -42,14 +25,17 @@ else if (budgetDay < 0) {
     console.log('Что-то пошло не так');
 } 
 
+console.log(budgetMonth);
+console.log('Миссия будет достигнута за ' + Math.ceil(mission / budgetMonth) + ' месяцев');
 console.log('Бюджет на день ' + Math.floor(budgetDay));
-showTypeOf(money);
-showTypeOf(income);
-showTypeOf(deposit);
-console.log(getExpensesMonth());
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(income.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
 console.log(addExpenses.toLowerCase().split(', '));
-console.log(getTargetMonth());
-getStatusIncome();
+
 
 
 
